49a8d65429017d53328fd960d435ac3609f32ea1
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-08-03 14:09:58 -0700

    formatting

--- hotstuff/vm/vm_control_interface.cc
+++ hotstuff/vm/vm_control_interface.cc
@@ -1,75 +1,103 @@
 #include "hotstuff/vm/vm_control_interface.h"
 #include "hotstuff/hotstuff_debug_macros.h"
-namespace hotstuff 
+namespace hotstuff
 {
+
+VMControlInterface::VMControlInterface(std::shared_ptr<VMBase> vm)
+    : utils::AsyncWorker()
+    , vm_instance(vm)
+    , blocks_to_validate()
+    , proposal_buffer()
+    , additional_proposal_requests(0)
+    , is_proposer(false)
+    , highest_committed_id(std::nullopt)
+{
+    start_async_thread([this] { run(); });
+}
+
+VMControlInterface::~VMControlInterface()
+{
+    end_async_thread();
+}
 
 void
 VMControlInterface::set_proposer()
 {
-	std::lock_guard lock(mtx);
-	HOTSTUFF_INFO("VM INTERFACE: entering proposer mode");
-	is_proposer = true;
+    std::lock_guard lock(mtx);
+    HOTSTUFF_INFO("VM INTERFACE: entering proposer mode");
+    is_proposer = true;
 }
 
 VMControlInterface::proposal_buffer_t
 VMControlInterface::get_proposal()
 {
-	std::unique_lock lock(mtx);
-	HOTSTUFF_INFO("VM INTERFACE: start get_proposal");
-	if (!is_proposer) {
-		HOTSTUFF_INFO("VM INTERFACE: not in proposer mode, returning empty proposal");
-		return nullptr;
-	}
+    std::unique_lock lock(mtx);
+    HOTSTUFF_INFO("VM INTERFACE: start get_proposal");
+    if (!is_proposer)
+    {
+        HOTSTUFF_INFO(
+            "VM INTERFACE: not in proposer mode, returning empty proposal");
+        return nullptr;
+    }
 
-	if (done_flag) return nullptr;
+    if (done_flag)
+        return nullptr;
 
-	if (proposal_buffer.empty()) {
-		if (additional_proposal_requests < PROPOSAL_BUFFER_TARGET) {
-			additional_proposal_requests = PROPOSAL_BUFFER_TARGET;
-		}
-		if (PROPOSAL_BUFFER_TARGET == 0) {
-			HOTSTUFF_INFO("VM INTERFACE: cannot get proposal from empty buffer after proposals are stopped");
-			return nullptr;
-		}
-		cv.notify_all();
-		HOTSTUFF_INFO("VM INTERFACE: waiting on new proposal from vm");
-		cv.wait(lock, [this] { return done_flag || (!proposal_buffer.empty());});
-	}
+    if (proposal_buffer.empty())
+    {
+        if (additional_proposal_requests < PROPOSAL_BUFFER_TARGET)
+        {
+            additional_proposal_requests = PROPOSAL_BUFFER_TARGET;
+        }
+        if (PROPOSAL_BUFFER_TARGET == 0)
+        {
+            HOTSTUFF_INFO("VM INTERFACE: cannot get proposal from empty buffer "
+                          "after proposals are stopped");
+            return nullptr;
+        }
+        cv.notify_all();
+        HOTSTUFF_INFO("VM INTERFACE: waiting on new proposal from vm");
+        cv.wait(lock,
+                [this] { return done_flag || (!proposal_buffer.empty()); });
+    }
 
-	if (done_flag) return nullptr;
+    if (done_flag)
+        return nullptr;
 
-	HOTSTUFF_INFO("VM INTERFACE: got new proposal from vm");
+    HOTSTUFF_INFO("VM INTERFACE: got new proposal from vm");
 
-	auto out = std::move(proposal_buffer.front());
-	proposal_buffer.erase(proposal_buffer.begin());
+    auto out = std::move(proposal_buffer.front());
+    proposal_buffer.erase(proposal_buffer.begin());
 
-	additional_proposal_requests 
-		= (proposal_buffer.size() > PROPOSAL_BUFFER_TARGET)
-			? 0 
-			: PROPOSAL_BUFFER_TARGET - proposal_buffer.size();
-	if (additional_proposal_requests > 0) {
-		cv.notify_all();
-	}
-	return out;
+    additional_proposal_requests
+        = (proposal_buffer.size() > PROPOSAL_BUFFER_TARGET)
+              ? 0
+              : PROPOSAL_BUFFER_TARGET - proposal_buffer.size();
+    if (additional_proposal_requests > 0)
+    {
+        cv.notify_all();
+    }
+    return out;
 }
 
 void
-VMControlInterface::clear_proposal_settings() {
-	is_proposer = false;
-	proposal_buffer.clear();
-	additional_proposal_requests = 0;
+VMControlInterface::clear_proposal_settings()
+{
+    is_proposer = false;
+    proposal_buffer.clear();
+    additional_proposal_requests = 0;
 }
 
 void
 VMControlInterface::submit_block_for_exec(submission_t submission)
 {
-	std::lock_guard lock(mtx);
-	
-	clear_proposal_settings();
+    std::lock_guard lock(mtx);
 
-	blocks_to_validate.push_back(std::move(submission));
+    clear_proposal_settings();
 
-	cv.notify_all();
+    blocks_to_validate.push_back(std::move(submission));
+
+    cv.notify_all();
 }
 
 //! log_commitment should be called in order in which things are committed
@@ -78,64 +106,72 @@ VMControlInterface::submit_block_for_exec(submission_t submission)
 //! (if, for example, this method gets called twice in succession
 //! before the run() thread wakes on the cv).
 void
-VMControlInterface::log_commitment(VMBlockID const& block_id) {
-	std::lock_guard lock(mtx);
+VMControlInterface::log_commitment(VMBlockID const& block_id)
+{
+    std::lock_guard lock(mtx);
 
-	if (block_id) {
-		highest_committed_id = block_id;
-	}
-	if (highest_committed_id) {
-		cv.notify_all();
-	}
+    if (block_id)
+    {
+        highest_committed_id = block_id;
+    }
+    if (highest_committed_id)
+    {
+        cv.notify_all();
+    }
 }
 
-
 void
-VMControlInterface::run() {
-	HOTSTUFF_INFO("VM INTERFACE: start run()");
-	while(true) {
-		std::unique_lock lock(mtx);
+VMControlInterface::run()
+{
+    HOTSTUFF_INFO("VM INTERFACE: start run()");
+    while (true)
+    {
+        std::unique_lock lock(mtx);
 
-		if ((!done_flag) && (!exists_work_to_do())) {
-			cv.wait(
-				lock, 
-				[this] () {
-					return done_flag || exists_work_to_do();
-				});
-		}
+        if ((!done_flag) && (!exists_work_to_do()))
+        {
+            cv.wait(lock,
+                    [this]() { return done_flag || exists_work_to_do(); });
+        }
 
-		if (done_flag) return;
+        if (done_flag)
+            return;
 
-		while (!blocks_to_validate.empty()) {
-			auto block_to_validate = std::move(blocks_to_validate.front());
-			blocks_to_validate.erase(blocks_to_validate.begin());
+        while (!blocks_to_validate.empty())
+        {
+            auto block_to_validate = std::move(blocks_to_validate.front());
+            blocks_to_validate.erase(blocks_to_validate.begin());
 
-			//block_to_validate is nullptr iff parsing failed.
-			//In this case, vm doesn't do anything.
-			if (block_to_validate) {
-				// exec_block is responsible for reverting any speculative state
-				// left over from calls to propose()
-				HOTSTUFF_INFO("VM INTERFACE: start exec_block");
-				vm_instance -> exec_block(*block_to_validate);
-				HOTSTUFF_INFO("VM INTERFACE: end exec block");
-			}
-		} 
-		if (highest_committed_id) {
-			vm_instance -> log_commitment(*highest_committed_id);
-			highest_committed_id = std::nullopt;
-		}
-		if (additional_proposal_requests > 0) {
-			//get additional proposals
-			if (!is_proposer) {
-				throw std::runtime_error("vm thread woke up early");
-			}
+            // block_to_validate is nullptr iff parsing failed.
+            // In this case, vm doesn't do anything.
+            if (block_to_validate)
+            {
+                // exec_block is responsible for reverting any speculative state
+                // left over from calls to propose()
+                HOTSTUFF_INFO("VM INTERFACE: start exec_block");
+                vm_instance->exec_block(*block_to_validate);
+                HOTSTUFF_INFO("VM INTERFACE: end exec block");
+            }
+        }
+        if (highest_committed_id)
+        {
+            vm_instance->log_commitment(*highest_committed_id);
+            highest_committed_id = std::nullopt;
+        }
+        if (additional_proposal_requests > 0)
+        {
+            // get additional proposals
+            if (!is_proposer)
+            {
+                throw std::runtime_error("vm thread woke up early");
+            }
 
-			// vm->propose() leaves vm in speculative future state.
-			proposal_buffer.emplace_back(vm_instance -> propose());
-			additional_proposal_requests--;
-		}
-		cv.notify_all();
-	}
+            // vm->propose() leaves vm in speculative future state.
+            proposal_buffer.emplace_back(vm_instance->propose());
+            additional_proposal_requests--;
+        }
+        cv.notify_all();
+    }
 }
 
 //! wait for the vm to finish whatever it's doing.
@@ -146,18 +182,17 @@ VMControlInterface::run() {
 //! should not be executed while this runs.
 //! In this implementation, this property is guaranteed
 //! by the fact that apply_block is not called
-//! concurrently with notify_vm_of_commitment 
+//! concurrently with notify_vm_of_commitment
 //! (in the vm bridge)
 //! and get_and_apply_next_proposal and apply_block
 //! both acquire a lock on speculation_map.
 //! TODO this arrangement is fragile, and would be easy
 //! to accidentally break.
 void
-VMControlInterface::finish_work_and_force_rewind() {
-	wait_for_async_task();
-	vm_instance -> rewind_to_last_commit();
+VMControlInterface::finish_work_and_force_rewind()
+{
+    wait_for_async_task();
+    vm_instance->rewind_to_last_commit();
 }
 
-
-
-} /* hotstuff */
+} // namespace hotstuff
--- hotstuff/vm/vm_control_interface.h
+++ hotstuff/vm/vm_control_interface.h
@@ -1,97 +1,91 @@
 #pragma once
 
-#include "utils/async_worker.h"
 #include "hotstuff/vm/vm_base.h"
+#include "utils/async_worker.h"
 
 #include <cstdint>
 #include <memory>
 #include <mutex>
 #include <vector>
 
-namespace hotstuff {
+namespace hotstuff
+{
 
 class LogAccessWrapper;
 
 /**
  * Wraps the virtual machine in an asynchronous interface
  * that buffers requests in and out of the machine.
- * 
+ *
  * Manages a proposal buffer and a buffer of decided blocks
  * that the virtual machine must handle.
- * 
+ *
  */
-class VMControlInterface : public utils::AsyncWorker {
-	std::shared_ptr<VMBase> vm_instance;
+class VMControlInterface : public utils::AsyncWorker
+{
+    std::shared_ptr<VMBase> vm_instance;
 
-	using utils::AsyncWorker::mtx;
-	using utils::AsyncWorker::cv;
+    using utils::AsyncWorker::cv;
+    using utils::AsyncWorker::mtx;
 
-	using proposal_buffer_t = std::unique_ptr<VMBlock>;
-	using submission_t = std::unique_ptr<VMBlock>;
+    using proposal_buffer_t = std::unique_ptr<VMBlock>;
+    using submission_t = std::unique_ptr<VMBlock>;
 
-	size_t PROPOSAL_BUFFER_TARGET = 3;
+    size_t PROPOSAL_BUFFER_TARGET = 3;
 
-	std::vector<submission_t> blocks_to_validate;
+    std::vector<submission_t> blocks_to_validate;
 
-	std::vector<proposal_buffer_t> proposal_buffer;
+    std::vector<proposal_buffer_t> proposal_buffer;
 
-	size_t additional_proposal_requests;
+    size_t additional_proposal_requests;
 
-	bool is_proposer;
+    bool is_proposer;
 
-	std::optional<VMBlockID> highest_committed_id;
+    std::optional<VMBlockID> highest_committed_id;
 
-	void clear_proposal_settings();
+    void clear_proposal_settings();
 
-	//vm runner side
-	void run();
+    // vm runner side
+    void run();
 
-	bool exists_work_to_do() override final {
-		return (blocks_to_validate.size() > 0)
-			|| ((additional_proposal_requests > 0) && is_proposer)
-			|| ((bool) highest_committed_id);
-	}
+    bool exists_work_to_do() override final
+    {
+        return (blocks_to_validate.size() > 0)
+               || ((additional_proposal_requests > 0) && is_proposer)
+               || ((bool)highest_committed_id);
+    }
 
 public:
+    VMControlInterface(std::shared_ptr<VMBase> vm);
+    ~VMControlInterface();
 
-	VMControlInterface(std::shared_ptr<VMBase> vm)
-		: utils::AsyncWorker()
-		, vm_instance(vm)
-		, blocks_to_validate()
-		, proposal_buffer()
-		, additional_proposal_requests(0)
-		, is_proposer(false)
-		, highest_committed_id(std::nullopt)
-		{
-			start_async_thread([this] {run();});
-		}
+    // from hotstuff side
+    void set_proposer();
+    proposal_buffer_t get_proposal();
+    void submit_block_for_exec(submission_t submission);
+    void log_commitment(VMBlockID const& block_id);
+    void finish_work_and_force_rewind();
 
-	~VMControlInterface() {
-		end_async_thread();
-	}
+    // call before any usage
+    void init_clean() { vm_instance->init_clean(); }
+    void init_from_disk(LogAccessWrapper const& decided_block_cache)
+    {
+        vm_instance->init_from_disk(decided_block_cache);
+    }
 
-	// from hotstuff side
-	void set_proposer();
-	proposal_buffer_t get_proposal();
-	void submit_block_for_exec(submission_t submission);
-	void log_commitment(VMBlockID const& block_id);
-	void finish_work_and_force_rewind();
+    // Called by external controls (i.e. from main logic component)
+    bool proposal_buffer_is_empty() const
+    {
+        std::lock_guard lock(mtx);
+        return proposal_buffer.empty() && (PROPOSAL_BUFFER_TARGET == 0)
+               && (additional_proposal_requests == 0);
+    }
 
-	//call before any usage
-	void init_clean() {
-		vm_instance -> init_clean();
-	}
-	void init_from_disk(LogAccessWrapper const& decided_block_cache) {
-		vm_instance -> init_from_disk(decided_block_cache);
-	}
-	bool proposal_buffer_is_empty() const {
-		std::lock_guard lock(mtx);
-		return proposal_buffer.empty() && (PROPOSAL_BUFFER_TARGET == 0) && (additional_proposal_requests == 0);
-	}
-	void stop_proposals() {
-		std::lock_guard lock(mtx);
-		PROPOSAL_BUFFER_TARGET = 0;
-	}
+    void stop_proposals()
+    {
+        std::lock_guard lock(mtx);
+        PROPOSAL_BUFFER_TARGET = 0;
+    }
 };
 
-} /* hotstuff */
+} // namespace hotstuff
